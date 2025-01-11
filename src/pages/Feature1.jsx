eb195f8a71d5619901cf30ab62d73ec9c0b2ba3b
ARK018 <kambliakshay22@gmail.com>
2025-01-12 03:48:43 +0530

    first commit

---
+++ src/pages/Feature1.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Feature1 = () => {
+  return (
+    <div className="h-screen flex justify-center items-center">
+      <h1>FEATURE 1</h1>
+    </div>
+  );
+};
+
+export default Feature1;
---
+++ src/pages/Feature2.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Feature2 = () => {
+  return (
+    <div className="h-screen flex justify-center items-center">
+      <h1>FEATURE 2</h1>
+    </div>
+  );
+};
+
+export default Feature2;
